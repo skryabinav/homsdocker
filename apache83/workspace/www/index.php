b07230388a3b524fceb6969a5b24ea13b0418cb2
Скрябин Алексей <skryabinav@gmail.com>
2024-03-23 12:01:24 +0300

    Change default image version

---
+++ apache83/workspace/www/index.php
@@ -0,0 +1,16 @@
+<?php
+phpinfo();
+$link = mysqli_connect("bitrixmysql", "user", "1234", "bitrix");
+
+if (!$link) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Соединение с MySQL установлено!" . PHP_EOL;
+echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
